92ba4ff5ded8a4cd152bf84943c27146e387fb5b
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-10-05 20:39:16 +0200

    Coins Collectors
    Juego Coins Collector

---
+++ CoinsCollector/Assets/Scripts/Coin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	public static int coinsCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		Coin.coinsCount++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.CompareTag ("Player")) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnDestroy ()
+	{
+		Coin.coinsCount--;
+		if (Coin.coinsCount <= 0)
+		{
+			GameObject timer = GameObject.Find(("GameTimer"));
+			Destroy(timer);
+
+			GameObject[] Fireworks = GameObject.FindGameObjectsWithTag("Firework");
+			foreach (GameObject firework in Fireworks)
+			{
+				firework.GetComponent<ParticleSystem>().Play();
+			}
+		}
+	}
+}
---
+++ CoinsCollector/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+	public float maxTime = 90.0f;
+	private float countdown = 0.0f;
+	
+	// Use this for initialization
+	void Start () {
+		countdown = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown -= Time.deltaTime;
+		if (countdown <= 0) {
+			Coin.coinsCount = 0;
+			SceneManager.LoadScene ("MainScene");
+		}
+	}
+}
